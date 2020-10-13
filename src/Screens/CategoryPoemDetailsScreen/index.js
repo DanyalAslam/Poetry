5820662ba8c0971a26f4ba4f0702bfa2a522c212
hurali97 <hurali97@gmail.com>
2020-10-13 23:48:05 +0500

    v 10 uploaded to store

--- src/Screens/CategoryPoemDetailsScreen/index.js
+++ src/Screens/CategoryPoemDetailsScreen/index.js
@@ -31,12 +31,12 @@ class CategoryPoemDetailsScreen extends React.Component {
 
     componentDidMount() {
 
-        AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId]);
+        // AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId]);
         // AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/8691691433'); //google test ad
 
-        // AdMobInterstitial.setAdUnitID('ca-app-pub-8059419171547646/8398110094')
+        AdMobInterstitial.setAdUnitID('ca-app-pub-8059419171547646/8398110094')
 
-        // this.showInterstitial()
+        this.showInterstitial()
 
 
 
@@ -432,9 +432,9 @@ class CategoryPoemDetailsScreen extends React.Component {
                 <AdMobBanner
                     style={{ margin: 2 * vh, height: 15 * vh, zIndex: 100, alignSelf: 'center' }}
                     adSize="banner"
-                    // adUnitID="ca-app-pub-8059419171547646/7788864330"
-                    adUnitID="ca-app-pub-3940256099942544/6300978111"  //google testad
-                    testDeviceID="EMULATOR"
+                    adUnitID="ca-app-pub-8059419171547646/7788864330"
+                    // adUnitID="ca-app-pub-3940256099942544/6300978111"  //google testad
+                    // testDeviceID="EMULATOR"
 
                 />
 
--- src/Screens/PoetPoemDetailScreen/index.js
+++ src/Screens/PoetPoemDetailScreen/index.js
@@ -38,9 +38,9 @@ class PoetPoemDetailScreen extends React.Component {
         // AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId]);
         // AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/8691691433'); //google test ad
 
-        // AdMobInterstitial.setAdUnitID('ca-app-pub-8059419171547646/5607523744');
+        AdMobInterstitial.setAdUnitID('ca-app-pub-8059419171547646/5607523744');
 
-        // this.showInterstitial()
+        this.showInterstitial()
 
 
         this.props.navigation.addListener("focus", () => {
@@ -532,9 +532,9 @@ class PoetPoemDetailScreen extends React.Component {
                     style={{ margin: 2 * vh, height: 15 * vh, zIndex: 100, alignSelf: 'center' }}
                     adSize="banner"
                     onAdFailedToLoad={(e) => console.log(e)}
-                    adUnitID="ca-app-pub-3940256099942544/6300978111" //google testad
-                    // adUnitID="ca-app-pub-8059419171547646/7352367170"
-                    testDeviceID="EMULATOR"
+                    // adUnitID="ca-app-pub-3940256099942544/6300978111" //google testad
+                    adUnitID="ca-app-pub-8059419171547646/7352367170"
+                    // testDeviceID="EMULATOR"
 
                 />
 
